151faa44cb6bbf229bca8907cba6aae63fc85b3a
Do Thinh <21130187@st.hcmuaf.edu.vn>
2025-05-16 19:12:11 +0700

    create service and repository

--- src/main/java/com/bookexchange/service/CategoryService.java
+++ src/main/java/com/bookexchange/service/CategoryService.java
@@ -1,12 +1,24 @@
 package com.bookexchange.service;
 
+import com.bookexchange.dto.request.CategoryCreationRequest;
+import com.bookexchange.dto.request.CategoryRequest;
+import com.bookexchange.dto.request.CategoryUpdateRequest;
+import com.bookexchange.dto.response.CategoryManagementResponse;
 import com.bookexchange.dto.response.CategoryResponse;
+import com.bookexchange.entity.Category;
+import com.bookexchange.entity.User;
+import com.bookexchange.exception.AppException;
+import com.bookexchange.exception.ErrorCode;
+import com.bookexchange.mapper.CategoryMapper;
 import com.bookexchange.repository.CategoryRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
@@ -17,6 +29,7 @@ import java.util.List;
 public class CategoryService {
 
     CategoryRepository categoryRepository;
+    CategoryMapper categoryMapper;
 
     public List<CategoryResponse> getAllCategories() {
 
@@ -27,4 +40,39 @@ public class CategoryService {
                         .build())
                 .toList();
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public CategoryManagementResponse createCategory(CategoryCreationRequest request) {
+        Category category = categoryMapper.toCategory(request);
+        return categoryMapper.toCategoryManagementResponse(categoryRepository.save(category));
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    public CategoryManagementResponse getCategory(String id) {
+        return categoryMapper.toCategoryManagementResponse(
+                categoryRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED)));
+    }
+
+    public Page<CategoryManagementResponse> getCategories(Pageable pageable) {
+        return categoryRepository.findAll(pageable)
+                .map(categoryMapper::toCategoryManagementResponse);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public CategoryManagementResponse updateCategory(String id, CategoryUpdateRequest request) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.CATEGORY_NOT_FOUND));
+
+        categoryMapper.updateCategory(category, request);
+        return categoryMapper.toCategoryManagementResponse(categoryRepository.save(category));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deleteCategory(String id) {
+        if (!categoryRepository.existsById(id)) {
+            throw new AppException(ErrorCode.CATEGORY_NOT_FOUND);
+        }
+        categoryRepository.deleteById(id);
+    }
+
+
 }
