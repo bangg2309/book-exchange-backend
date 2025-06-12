80c4aa572d194fa3c8ae2002f0e847e826891389
Do Thinh <21130187@st.hcmuaf.edu.vn>
2025-06-12 11:36:50 +0700

    complete book management

---
+++ src/main/java/com/bookexchange/dto/response/BookManagementResponse.java
@@ -0,0 +1,30 @@
+package com.bookexchange.dto.response;
+
+import com.bookexchange.entity.ListedBook;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BookManagementResponse {
+    long id;
+    String title;
+    BigDecimal priceNew;
+    BigDecimal price;
+    int conditionNumber;
+    String description;
+    String thumbnail;
+    String publisher;
+    String school;
+    String name;
+    List<AuthorResponse> author;
+}
