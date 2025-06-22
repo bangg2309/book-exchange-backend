4349a896189f180668db9b3e4bebf097a57276c7
Tran Qui Bang <21130279@st.hcmuaf.edu.vn>
2025-06-22 20:49:57 +0700

    feat: implement feature order management

--- src/main/java/com/bookexchange/constant/PredefineOrder.java
+++ src/main/java/com/bookexchange/constant/PredefineOrder.java
@@ -1,12 +1,11 @@
 package com.bookexchange.constant;
 
 public class PredefineOrder {
-    public static final int PAYMENT_ORDER_FAILED = 0;
+    public static final int PAYMENT_ORDER_FAILED = -1;
+    public static final int PAYMENT_ORDER_PENDING = 0;
     public static final int PAYMENT_ORDER_SUCCESS = 1;
-    public static final int PAYMENT_ORDER_PENDING = 2;
-    public static final int PAYMENT_ORDER_EXPIRED = 3;
-    public static final int PAYMENT_ORDER_CANCELED = 4;
-    public static final int PAYMENT_ORDER_REFUNDED = 5;
+    public static final int PAYMENT_ORDER_CANCELED = 2;
+    public static final int PAYMENT_ORDER_REFUNDED = 3;
 
 
     private PredefineOrder() {
